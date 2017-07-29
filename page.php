d33b908a477ec9eedff881dd80547a8e03c87fd6
Roni Laukkarinen <roni@laukkarinen.info>
2017-07-30 02:17:26 +0300

    Add article width for default page

--- page.php
+++ page.php
@@ -17,7 +17,7 @@ get_header(); ?>
 <div id="primary" class="content-area">
 	<main id="main" class="site-main">
 
-    <div class="container">
+    <div class="container container-article">
       <?php while ( have_posts() ) {
       	the_post(); ?>
 
