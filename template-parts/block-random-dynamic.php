4f7f6c69bb838c1b773ceb81c3ed08e73e7cdd57
Roni Laukkarinen <roni@laukkarinen.info>
2017-07-30 01:18:42 +0300

    Add GitHub commits to the footer, indent some files

--- template-parts/block-random-dynamic.php
+++ template-parts/block-random-dynamic.php
@@ -30,36 +30,36 @@ $loop = new WP_Query( $args );
 if ( $loop->have_posts() ) : ?>
 
 <?php while ( $loop->have_posts() ) :
-    $loop->the_post();
+$loop->the_post();
 
-    if ( has_post_thumbnail() ) :
-      $post_thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'large' )[0];
-    else :
-      $post_thumbnail = khonsu_get_random_image_url();
-    endif;
-    ?>
+if ( has_post_thumbnail() ) :
+	$post_thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'large' )[0];
+else :
+	$post_thumbnail = khonsu_get_random_image_url();
+endif;
+?>
 
-    <div class="entry">
-      <div class="entry-featured-image" style="background-image:url('<?php echo $post_thumbnail; ?>');"><a href="<?php echo get_the_permalink(); ?>" class="absolute-link"><span class="screen-reader-text">Link to article "<?php echo get_the_title(); ?>"</span></a></div>
+<div class="entry">
+	<div class="entry-featured-image" style="background-image:url('<?php echo $post_thumbnail; ?>');"><a href="<?php echo get_the_permalink(); ?>" class="absolute-link"><span class="screen-reader-text">Link to article "<?php echo get_the_title(); ?>"</span></a></div>
 
-      <div class="entry-details">
-        <h3><a href="<?php echo get_the_permalink(); ?>"><?php echo get_the_title(); ?></a></h3>
-        <?php if ( has_excerpt() ) : ?>
-          <p><?php echo get_the_excerpt(); ?></p>
-        <?php else : ?>
-          <p>
-            <?php
-            $sentence = preg_match( '/^([^.!?]*[\.!?]+){0,3}/', strip_tags( get_the_content() ), $summary );
-            echo strip_shortcodes( $summary[0] );
-            ?>
-          </p>
-        <?php endif; ?>
+	<div class="entry-details">
+		<h3><a href="<?php echo get_the_permalink(); ?>"><?php echo get_the_title(); ?></a></h3>
+		<?php if ( has_excerpt() ) : ?>
+			<p><?php echo get_the_excerpt(); ?></p>
+		<?php else : ?>
+			<p>
+				<?php
+				$sentence = preg_match( '/^([^.!?]*[\.!?]+){0,3}/', strip_tags( get_the_content() ), $summary );
+				echo strip_shortcodes( $summary[0] );
+				?>
+			</p>
+		<?php endif; ?>
 
-        <div class="entry-details-footer">
-          <p><time datetime="<?php the_time( 'c' ); ?>"><?php the_time( 'j.' ); ?> <?php the_time( 'F' ); ?>ta <?php the_time( 'Y' ); ?></time> <span class="dot-divider"></span> <?php echo khonsu_estimated_reading_time(); ?></p>
-        </div><!-- .entry-footer -->
+		<div class="entry-details-footer">
+			<p><time datetime="<?php the_time( 'c' ); ?>"><?php the_time( 'j.' ); ?> <?php the_time( 'F' ); ?>ta <?php the_time( 'Y' ); ?></time> <span class="dot-divider"></span> <?php echo khonsu_estimated_reading_time(); ?></p>
+		</div><!-- .entry-footer -->
 
-      </div><!-- .entry-details -->              
-    </div><!-- .entry -->
-  <?php endwhile;
+	</div><!-- .entry-details -->              
+</div><!-- .entry -->
+<?php endwhile;
 endif; ?>
--- template-parts/trakt.php
+++ template-parts/trakt.php
@@ -23,45 +23,45 @@ $trakt_cachetime = 10800; // Three hours
           copy( $trakt_url, $trakt_cachefile );
         }
 
-				// Serve from the cache if it is younger than $trakt_cachetime
+			  // Serve from the cache if it is younger than $trakt_cachetime
         if ( file_exists( $trakt_cachefile ) && time() - $trakt_cachetime < filemtime( $trakt_cachefile ) ) :
-            // Do nothing, it's cached
+         // Do nothing, it's cached
         else :
-            // If time ran out, copy over
-        	copy( $trakt_url, $trakt_cachefile );
+         // If time ran out, copy over
+         copy( $trakt_url, $trakt_cachefile );
         endif;
 
-				$rss = simplexml_load_file( $trakt_cachefile );
-				$list = $rss->xpath( '//@url' );
-				$prepared_urls = array();
+        $rss = simplexml_load_file( $trakt_cachefile );
+        $list = $rss->xpath( '//@url' );
+        $prepared_urls = array();
 
-				foreach ( $list as $item ) {
-					$item = parse_url( $item );
-					$prepared_urls[] = $item['scheme'] . '://' . $item['host'] . '' . $item['path'] . '';
-				}
+        foreach ( $list as $item ) {
+          $item = parse_url( $item );
+          $prepared_urls[] = $item['scheme'] . '://' . $item['host'] . '' . $item['path'] . '';
+        }
 
-				$image = $prepared_urls[1];
-				$trakt_image_filename = basename( $image );
+        $image = $prepared_urls[1];
+        $trakt_image_filename = basename( $image );
 
-				if ( getenv( 'WP_ENV' ) === 'development' || file_exists( dirname( __FILE__ ) . '/.dev' ) ) {
-					$paikallinen_traktkuva = '/var/www/rollemaa/trakt-image-db/' . $trakt_image_filename;
-				} else {
-					$paikallinen_traktkuva = '/var/www/rollemaa.org/public_html/trakt-image-db/' .$trakt_image_filename;
-				}
+        if ( getenv( 'WP_ENV' ) === 'development' || file_exists( dirname( __FILE__ ) . '/.dev' ) ) {
+          $paikallinen_traktkuva = '/var/www/rollemaa/trakt-image-db/' . $trakt_image_filename;
+        } else {
+          $paikallinen_traktkuva = '/var/www/rollemaa.org/public_html/trakt-image-db/' .$trakt_image_filename;
+        }
 
-				copy( $image, $paikallinen_traktkuva );
-				?>
+        copy( $image, $paikallinen_traktkuva );
+        ?>
 
-				<div class="traktposter">
-					<a href="http://trakt.tv/users/rolle"><img class="fit-image" src="<?php echo get_home_url(); ?>/trakt-image-db/<?php echo $trakt_image_filename; ?>" alt="Trakt" /></a>
-				</div>
+        <div class="traktposter">
+          <a href="http://trakt.tv/users/rolle"><img class="fit-image" src="<?php echo get_home_url(); ?>/trakt-image-db/<?php echo $trakt_image_filename; ?>" alt="Trakt" /></a>
+        </div>
 
-				<div class="info-overlay">
-					<h3><a href="<?php echo $rss->entry->link->attributes()->href; ?>"><?php echo $rss->entry->title; ?></a></h3>
-				</div>
+        <div class="info-overlay">
+          <h3><a href="<?php echo $rss->entry->link->attributes()->href; ?>"><?php echo $rss->entry->title; ?></a></h3>
+        </div>
 
-			<?php endif; ?>
-		</div><!-- .feed-item -->
+      <?php endif; ?>
+    </div><!-- .feed-item -->
 
-	</div><!-- .service -->
+  </div><!-- .service -->
 </div><!-- .col-->
