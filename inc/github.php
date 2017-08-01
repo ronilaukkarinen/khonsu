30949d79f736f2bd140cf33b77551a1c05687ce9
Roni Laukkarinen <roni@laukkarinen.info>
2017-08-01 19:12:41 +0300

    Improve github integration

--- inc/github.php
+++ inc/github.php
@@ -7,24 +7,22 @@
 
 $github_url = 'https://github.com/ronilaukkarinen/khonsu/commits/master.atom';
 $github_cachefile = get_theme_file_path( 'inc/cache/github.xml' );
-$github_cachetime = 10800; // Three hours
+$github_cachetime = 1800;
 ?>
 
 <?php
 if ( strpos( file_get_contents( $github_url ), '<?xml' ) !== false ) :
 
-// If cache file does not exist, let's create it
-if ( ! file_exists( $github_cachefile ) ) {
-  copy( $github_url, $github_cachefile );
-}
+  // If cache file does not exist, let's create it
+  if ( ! file_exists( $github_cachefile ) ) {
+    touch( $github_cachefile );
+    copy( $github_url, $github_cachefile );
+  }
 
-// Serve from the cache if it is younger than $github_cachetime
-if ( file_exists( $github_cachefile ) && time() - $github_cachetime < filemtime( $github_cachefile ) ) :
-  // Do nothing, it's cached
-else :
-  // If time ran out, copy over
-  copy( $github_url, $github_cachefile );
-endif;
+  // If file is older than cache time, overwrite file
+  if ( time() - filemtime( $github_cachefile ) > 2 * $github_cachetime ) {
+    copy( $github_url, $github_cachefile );
+  }
 
   $rss = simplexml_load_file( $github_cachefile );
   $updated_processed = explode( 'T', $rss->entry->updated );
