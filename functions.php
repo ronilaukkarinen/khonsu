9965c713d0377d106a15386543790345d6035639
Roni Laukkarinen <roni@laukkarinen.info>
2017-08-09 11:35:48 +0300

    Add more versatile ads

--- functions.php
+++ functions.php
@@ -76,10 +76,52 @@ if ( in_the_loop() ) {
       }
 
       if ( 0 === $key_total ) {
-        $adfile = file_get_contents( get_theme_file_path( '/template-parts/ads-google-middle.php' ) );
-        $ad = array( 'ad1' => $adfile );
+        $ad = array( 'ad1' => '<div class="keskimainos-desktop keskimainos">
+  <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+  <!-- Jättibanneri keskellä (728x90) -->
+  <ins class="adsbygoogle"
+  style="display:inline-block;width:728px;height:90px"
+  data-ad-client="ca-pub-8523880252818258"
+  data-ad-slot="1607949247"></ins>
+  <script>
+    (adsbygoogle = window.adsbygoogle || []).push({});
+  </script>
+</div>
+
+<div class="keskimainos-mobile keskimainos">
+  <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+  <!-- Mobiilibanneri -->
+  <ins class="adsbygoogle"
+  style="display:inline-block;width:320px;height:100px"
+  data-ad-client="ca-pub-8523880252818258"
+  data-ad-slot="7514882041"></ins>
+  <script>
+    (adsbygoogle = window.adsbygoogle || []).push({});
+  </script>
+</div>
+
+<div class="keskimainos-ipad keskimainos">
+  <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+  <!-- Keskikokoinen mainos (iPad) -->
+  <ins class="adsbygoogle"
+  style="display:inline-block;width:468px;height:60px"
+  data-ad-client="ca-pub-8523880252818258"
+  data-ad-slot="5898548047"></ins>
+  <script>
+    (adsbygoogle = window.adsbygoogle || []).push({});
+  </script>
+</div>' );
       } else {
-        $ad = array( 'ad2' => $adfile );
+        $ad = array( 'ad2' => '<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<ins class="adsbygoogle"
+     style="display:block; text-align:center;"
+     data-ad-format="fluid"
+     data-ad-layout="in-article"
+     data-ad-client="ca-pub-8523880252818258"
+     data-ad-slot="6978444070"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>' );
       }
 
       foreach ( $total as $key_para => $para ) {
