1574c45ee1b93d812878ef78f362b4392c63f450
Roni Laukkarinen <roni@laukkarinen.info>
2017-08-09 10:43:06 +0300

    Advertising tweaks

--- header.php
+++ header.php
@@ -22,6 +22,13 @@
 <link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_template_directory_uri(); ?>/images/icon-retina.png">
 
 <?php wp_head(); ?>
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+  (adsbygoogle = window.adsbygoogle || []).push({
+    google_ad_client: "ca-pub-8523880252818258",
+    enable_page_level_ads: true
+  });
+</script>
 </head>
 
 <body <?php body_class(); ?>>
