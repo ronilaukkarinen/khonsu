5138068b15d2186cdde6c9be4ebf50ead553cf66
Roni Laukkarinen <roni@laukkarinen.info>
2018-02-12 17:43:08 +0200

    Add 404 for search page

--- search.php
+++ search.php
@@ -19,8 +19,8 @@ get_template_part('template-parts/hero', get_post_type()); ?>
       $count = 0; ?>
 
       <div class="notification-box">
-        <h3>Hakusi tulokset</h3>
-        <p>Hakemallasi hakusanalla <i><?php echo get_search_query(); ?></i> löytyi yhteensä  <?php $allsearch = new WP_Query("s=$s&showposts=-1"); $key = wp_specialchars($s, 1); $count = $allsearch->post_count; _e(''); echo $count; wp_reset_query(); ?> tulosta.</p>
+        <h3><?php _e( 'Hakusi tulokset', 'khonsu' ); ?></h3>
+        <p><?php _e( 'Hakemallasi hakusanalla', 'khonsu' ); ?> <i><?php echo get_search_query(); ?></i> <?php _e( 'löytyi yhteensä ', 'khonsu' ); $allsearch = new WP_Query("s=$s&showposts=-1"); $key = wp_specialchars($s, 1); $count = $allsearch->post_count; _e(''); echo $count; wp_reset_query(); _e( ' tulosta.', 'khonsu' ); ?></p>
       </div><!-- .notification-box -->
 
       <?php while ( have_posts() ) :
@@ -30,9 +30,28 @@ get_template_part('template-parts/hero', get_post_type()); ?>
 
     khonsu_pagination();
 
-  else :
-    get_template_part( 'template-parts/content', 'none' );
-    endif; ?>
+  else : ?>
+
+    <div class="notification-box">
+      <h3><?php _e( 'Mitään ei löytynyt', 'khonsu' ); ?></h3>
+      <p>Hakemallasi hakusanalla <i><?php echo get_search_query(); ?></i> <?php _e( 'ei löytynyt yhtikäs mitään.', 'khonsu' ); ?></p>
+    </div><!-- .notification-box -->
+
+      <div class="entry-content template-haku">
+
+        <h2><?php _e( 'Hae uudestaan!', 'khonsu' ); ?></h2>
+
+        <p><?php _e( 'Ei mittään hättää! Voit nimittäin etsiä uudella hakusanalla jokaisen '.wp_count_posts()->publish.' kirjoituksen joukosta. Ehkä uusintayritys tepsii.', 'khonsu' ); ?></p>
+
+        <form method="get" id="searchform" action="<?php bloginfo( 'home' ); ?>/">
+          <div><input type="text" value="<?php echo wp_specialchars( $s, 1 ); ?>" name="s" id="s" class="search" />
+            <input type="submit" id="searchsubmit" value="Etsi merkintöjä" class="button" />
+          </div>
+        </form>
+
+      </div><!-- .entry-content -->    
+
+  <?php endif; ?>
 
   </div><!-- .container -->
 
